cb8773e9d3bf4dce2b1af0adb7e857c549ca81f9
Muhammad Fauzan <muhammadfauzanfadhlulbarr@gmail.com>
2020-10-19 10:48:01 +0700

    commit

---
+++ Latihan6.java
@@ -0,0 +1,19 @@
+/**
+ * Latihan6
+ */
+public class Latihan6 {
+
+    public static int jumlahKambing;
+      
+    
+    
+}
+
+class KambingStatic {
+    public static final String NAMA_KAMBING = "MIDUN";
+    public static void main(String[] args) {
+        Latihan6.jumlahKambing = 485000;
+        System.out.println(NAMA_KAMBING + " memiliki kambing sebanyak " + Latihan6.jumlahKambing);;
+    }
+    
+}
